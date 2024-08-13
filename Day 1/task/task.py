8c621e80479036cef778e8da902e0fa718f6d94b
akxaalex16 <akxaalex16@gmail.com>
2024-08-13 13:21:46 -0700

    print, input, commenting, debugging, string manipulation, and variables

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print('Welcome to the Band Name Generator')
+city = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is your pet's name?\n")
+print(f'Your band name could be: {city} {pet_name}')
