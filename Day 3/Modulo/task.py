5a837153df5dc88030371a378268b89e3a2390bc
akxaalex16 <akxaalex16@gmail.com>
2024-08-13 20:37:26 -0700

    if/elif/else statements, nested if statements, modulo, logical operators

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,11 @@
+# 10 divided by 5 = 2, so the remainder is zero
+# 10 % 5 = 0
+
+print(10 % 3)
+
+num_to_check = int(input('print number: '))
+print(num_to_check % 2)
+if num_to_check % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,47 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+# price = 0
+# if size == "s":
+#     price += 15
+#     if pepperoni == 'y':
+#         price += 2
+#     if extra_cheese == 'y':
+#         price += 1
+# elif size == "m":
+#     price += 20
+#     if pepperoni == 'y':
+#         price += 3
+#     if extra_cheese == 'y':
+#         price += 1
+# elif size == 'l':
+#     price += 25
+#     if pepperoni == 'y':
+#         price += 3
+#     if extra_cheese == 'y':
+#         price += 1
+# else:
+#     print('Invalid size.')
+#
+# print(f'Your final bill is: ${price}')
+
+bill = 0
+if size == 'S':
+    bill += 15
+elif size == 'M':
+    bill += 20
+elif size == 'L':
+    bill += 25
+else:
+    print('You typed the wrong inputs.')
+
+if pepperoni == 'Y':
+    if size == 'S':
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == 'Y':
+    bill += 1
+
+print(f'Your final bill is : ${bill}.')
