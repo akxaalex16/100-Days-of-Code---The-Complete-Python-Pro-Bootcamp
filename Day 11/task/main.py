aedb11609de36a09682c9cade7eba1f894c7ef64
akxaalex16 <akxaalex16@gmail.com>
2024-08-27 20:26:13 -0700

    created function deal_card()
    returns a random card from the deck

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,11 +1,26 @@
 import random
 from art import logo
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 if_play = True
+
+
+def deal_card():
+    """Returns a random card from the deck"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    single_card = random.choice(cards)
+    return single_card
+
+
 user_cards = []
 computer_cards = []
-def deal_card():
-    return
+
+for _ in range(2):
+    new_card = deal_card()
+    user_cards.append(new_card)
+    # user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
 def calculate_score(cards_list):
     return cards_list
 
@@ -14,15 +29,6 @@ while if_play:
     play_yes_or_no = input("Do you want to play a game of Blackjack? Tpe 'y' or 'n': ")
     if play_yes_or_no == 'y':
         print(logo)
-        your_cards = random.choices(cards, k=2)
-        your_score = sum(your_cards)
-        dealer_card = random.choice(cards)
-        print(f"Your cards: {your_cards}, current score: {your_score}  ")
-        print(f"Computer's first card: {dealer_card}")
-        get_another_card = input("Type 'y' to get another card, type 'n' to pass: ")
-        if get_another_card == 'y':
-            your_other_card = random.choice(cards)
-            score_now = your_cards + your_other_card
-            print(f"Your cards: {your_cards, your_other_card}, current score: {score_now}")
-            print(f"Computer's first card: {dealer_card}")
+       
+
 
